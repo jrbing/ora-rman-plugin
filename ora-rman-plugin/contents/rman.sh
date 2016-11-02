43b7b19a80d0d5387aad568d0c24d922c962311a
JR Bing <jr@jrbing.com>
2016-11-02 15:38:47 -0400

    Added validation checks to see if script exists

--- ora-rman-plugin/contents/rman.sh
+++ ora-rman-plugin/contents/rman.sh
@@ -11,21 +11,47 @@
 
 set -e                          # Exit immediately on error
 
-#export NLS_LANG=AMERICAN_AMERICA.WE8ISO8859P15
-#export PATH=$PATH:/usr/local/bin:/usr/ccs/bin:/usr/sbin:/usr/ucb:/usr/openwin/bin:/etc:.
-
 # Required arguments
 oracle_sid=$1
 rman_cmdfile=$2
 
-# Check for Oracle SID
-if [[ $oracle_sid ]]; then
-  export ORACLE_SID=$oracle_sid
-  export ORAENV_ASK=NO
-  . oraenv
-else
-  printf "ORACLE_SID must be specified \n"
-  exit 1
-fi
+function echoinfo() {
+  local GC="\033[1;32m"
+  local EC="\033[0m"
+  printf "${GC} ☆  INFO${EC}: %s\n" "$@";
+}
 
-rman cmdfile="$rman_cmdfile"
+function echoerror() {
+  local RC="\033[1;31m"
+  local EC="\033[0m"
+  printf "${RC} ✖  ERROR${EC}: %s\n" "$@" 1>&2;
+  exit 1
+}
+
+function validate_oracle_sid() {
+  echoinfo "Validating ORACLE_SID"
+  if [[ $oracle_sid ]]; then
+    export ORACLE_SID=$oracle_sid
+    export ORAENV_ASK=NO
+    . oraenv
+  else
+    echoerror "ORACLE_SID must be specified"
+  fi
+}
+
+function validate_rman_cmdfile() {
+  if [[ -f $rman_cmdfile ]]; then
+    echoinfo "Found $rman_cmdfile"
+  else
+    echoerror "Could not find $rman_cmdfile"
+  fi
+}
+
+function run_rman_script() {
+  echoinfo "Executing $rman_cmdfile"
+  rman cmdfile="$rman_cmdfile"
+}
+
+validate_oracle_sid
+validate_rman_cmdfile
+run_rman_script
